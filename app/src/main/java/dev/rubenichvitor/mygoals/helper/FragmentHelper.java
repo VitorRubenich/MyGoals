63615ba10c014b3b176969f5040d114954283d04
VitorRubenich <rubenich.vitor@gmail.com>
2023-08-04 20:57:50 -0300

    feat FragmentHelper to less code

---
+++ app/src/main/java/dev/rubenichvitor/mygoals/helper/FragmentHelper.java
@@ -0,0 +1,32 @@
+package dev.rubenichvitor.mygoals.helper;
+
+import android.app.Activity;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+import dev.rubenichvitor.mygoals.R;
+
+public class FragmentHelper {
+    private FragmentHelper(){
+
+    }
+
+    public static void callNextFragment(Activity activity, Fragment fragment, int containerToReplace){
+        FragmentManager fragmentManager = ((FragmentActivity) activity).getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .addToBackStack("TAG")
+                .replace(containerToReplace, fragment)
+                .commit();
+    }
+
+    public static void callNextFragment(Activity activity, Fragment fragment){
+        FragmentManager fragmentManager = ((FragmentActivity) activity).getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .addToBackStack("TAG")
+                .replace(R.id.fragment_container_view_tag, fragment)
+                .commit();
+    }
+
+}
